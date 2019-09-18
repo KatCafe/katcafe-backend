c0f5bcfed19224e7fc5172574ec6e1c057eaa7e1
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-09-18 17:13:09 +0300

    captcha change

--- src/modules/DB/models/captcha/captcha.route.js
+++ src/modules/DB/models/captcha/captcha.route.js
@@ -3,6 +3,8 @@ const svgCaptcha = require('svg-captcha');
 import CryptoHelper from "src/modules/helpers/crypto-helper"
 import StringHelper from "modules/helpers/string-helper";
 
+const size = 4;
+
 export default function (express){
 
     express.get('/captcha/get', async function(req, res ) {
@@ -10,14 +12,16 @@ export default function (express){
         try{
 
             const captcha = svgCaptcha.create({
-                ignoreChars: '0o1ilI',
-                noise: 3,
+                ignoreChars: 'Oo1ilI',
+                noise: 1,
                 color: true,
+                size: size,
             });
 
             res.json( {
 
                 captcha: {
+                    size,
                     data: captcha.data,
                     encryption: CryptoHelper.encryptText( 0 + "#$#"+captcha.text + "#$#" + StringHelper.makeId(32) + "#$#" + new Date().getTime() ).toString("hex") ,
                 },
