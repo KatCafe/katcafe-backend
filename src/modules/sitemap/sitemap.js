d037e523997e6ab91ef4d6783063510b042ff996
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-10-02 17:59:57 +0300

    sitemap recurent

--- src/modules/sitemap/sitemap.js
+++ src/modules/sitemap/sitemap.js
@@ -3,20 +3,24 @@ import consts from 'consts/consts';
 import SitemapGenerator from 'sitemap-generator';
 
 
-
 class Sitemap{
 
     constructor(){
+
+        this._lastTime = 0;
+        this._finished = true;
 
         // create generator
         this._generator = SitemapGenerator(consts.domain, {
             filepath: './public/sitemap.xml',
             maxEntriesPerFile: 50000,
             changeFreq: 'always',
+            maxConcurrency: 1,
         });
 
         // register event listeners
         this._generator.on('done', () => {
+
             // sitemaps created
             this.sitemapCreated();
         });
@@ -27,11 +31,21 @@ class Sitemap{
         });
 
         // start the crawler
-        this._generator.start();
+        setInterval( ()=>{
+
+            if ( this._finished && new Date().getTime() - this._lastTime > 60*1000 ) {
+                this._finished = false;
+
+                console.log("Sitemap Starting");
+                this._generator.start();
+            }
+
+        }, 3000);
 
     }
 
     sitemapCreated(){
+        this._finished = true;
         console.log("Sitemap finished");
     }
 
