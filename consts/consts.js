ab87d2533b413539fbe9ebbbce5e5b503c11dc5a
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-10-02 23:23:11 +0300

    fix sitemap generation

--- consts/consts.js
+++ consts/consts.js
@@ -6,6 +6,6 @@ export default {
     PORT: debug ? 8085 : 2096,
     DOMAIN: debug ? "http://127.0.0.1:8080" : "https://katcafe.org",
 
-    SITEMAP: debug ? true : false,
+    SITEMAP: debug ? false : true,
 
 };
